cfb8bf09f8bad255624d73650f13af1d87ae36ed
TruongAnim <truonganim.dev@gmail.com>
2023-05-28 09:49:56 +0700

    add assets

--- lib/constants/asset_helper.dart
+++ lib/constants/asset_helper.dart
@@ -1,7 +1,8 @@
 class AssetHelper {
-  static const String assetRoot = 'assets/images';
-  static const String icon1 = '$assetRoot/icon1.png';
-  static const String icon2 = '$assetRoot/icon2.png';
-  static const String icon3 = '$assetRoot/icon3.png';
-  static const String icon4 = '$assetRoot/icon4.png';
+  static const String assetRootImages = 'assets/images';
+  static const String assetRootIcons = 'assets/icons';
+  static const String icon1 = '$assetRootIcons/icon1.png';
+  static const String icon2 = '$assetRootIcons/icon2.png';
+  static const String icon3 = '$assetRootIcons/icon3.png';
+  static const String icon4 = '$assetRootIcons/icon4.png';
 }
